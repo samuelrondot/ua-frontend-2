7f570ecc491527dd9acb816378c0cce25364e5f3
Samuel Rondot <samuel@MacBook-Pro.local>
2023-07-08 19:29:08 +0200

    feat: initial commit

--- src/modules/sales/capture/components/hero/video/index.tsx
+++ src/modules/sales/capture/components/hero/video/index.tsx
@@ -7,7 +7,7 @@ export const Video: FC = () => {
         <>
             <div className="absolute top-0 right-0 left-0 bottom-0">
                 <iframe
-                    src="https://player.vimeo.com/video/841660544?autoplay=1&loop=1&autopause=0"
+                    src="https://player.vimeo.com/video/843505540?autoplay=1&loop=1&autopause=0"
                     width="100%"
                     height="100%"
                     frameBorder={0}
